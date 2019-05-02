0ef3c301d0b5b2b8965195ec57dbde3c14b85770
Khalid Mohamed <kalmohammed56@gmail.com>
2019-05-02 14:53:37 +0300

    Added audio source and made it start working

---
+++ Assets/Rocket.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    AudioSource audiosource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProccessInput();
+    }
+
+    void ProccessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!audiosource.isPlaying)// So it doesnt layer
+            {
+                audiosource.Play();
+            }
+        }
+        else
+        {
+            audiosource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+            // rigidbody.AddRelativeForce(Vector3.left);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+            // rigidbody.AddRelativeForce(Vector3.right);
+        }
+    }
+}
