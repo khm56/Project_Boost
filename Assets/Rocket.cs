ef50d56d39a05ac8a6f8c07afbc4be029dcb99fb
Khalid Mohamed <kalmohammed56@gmail.com>
2019-05-03 21:36:05 +0300

    Added level 1 and 2 , 3 is in the process

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 100f;
     Rigidbody rigidbody;
     AudioSource audiosource;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                break;
+            default:
+                print("Shit");
+                break;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         audiosource = GetComponent<AudioSource>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        ProccessInput();
+        Thrust();
+        Rotate();
     }
-
-    void ProccessInput()
+    void Thrust()
     {
+        float thrustThisFrame = mainThrust * Time.deltaTime; ;
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!audiosource.isPlaying)// So it doesnt layer
             {
                 audiosource.Play();
@@ -33,15 +48,24 @@ public class Rocket : MonoBehaviour
         {
             audiosource.Stop();
         }
+    }
+    void Rotate()
+    {
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        rigidbody.freezeRotation = true; // take manual contreol rotation
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+
+            transform.Rotate(Vector3.forward * rotationThisFrame);
             // rigidbody.AddRelativeForce(Vector3.left);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
             // rigidbody.AddRelativeForce(Vector3.right);
         }
+
+        rigidbody.freezeRotation = false; //Resume physics control
     }
 }
